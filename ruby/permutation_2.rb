e623d8760aa644678cc775959cf563149cead60c
devninja67 <devninja67@gmail.com>
2019-01-18 17:41:11 +0100

    completed permutation_2

--- ruby/permutation_2.rb
+++ ruby/permutation_2.rb
@@ -8,6 +8,22 @@
 #            321
 
 N = ARGV[0].to_i == 0? 3 : ARGV[0].to_i
-flg = Array.new(N)
+$flg = Array.new(N + 1)
+$perm = String.new("")
 
-puts permutation 1
+def permutation(n)
+  if n > N
+    puts $perm
+    return
+  end
+  for i in 1..N
+    next if $flg[i]
+    $flg[i] = true
+    $perm << i.to_s
+    permutation n + 1
+    $flg[i] = false
+    $perm = $perm.slice(0..-2)
+  end
+end
+
+permutation 1
